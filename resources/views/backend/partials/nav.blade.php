e701315025e069594adb80f09218df99e971d49f
Sarwar Jahan Bhuiyan <31752675+sunjid@users.noreply.github.com>
2021-06-06 18:34:57 +0600

    Update Nav.blade.php

--- resources/views/backend/partials/nav.blade.php
+++ resources/views/backend/partials/nav.blade.php
@@ -72,12 +72,12 @@
       </li>
       <li class="nav-item dropdown d-none d-xl-inline-flex user-dropdown">
         <a class="nav-link dropdown-toggle" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
-          <img class="img-xs rounded-circle ml-2" src="{{ asset('images/faces/face8.jpg') }}" alt="Profile image"> <span class="font-weight-normal"> Henry Klein </span></a>
+          <img class="img-xs rounded-circle ml-2" src="{{ asset('images/faces/face8.jpg') }}" alt="Profile image"> <span class="font-weight-normal"> Sarwar Sunjid </span></a>
         <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
           <div class="dropdown-header text-center">
             <img class="img-md rounded-circle" src="{{ asset('images/faces/face8.jpg') }}" alt="Profile image">
             <p class="mb-1 mt-3">Sarwar Sunjid</p>
-            <p class="font-weight-light text-muted mb-0">allenmoreno@gmail.com</p>
+            <p class="font-weight-light text-muted mb-0">sarwarsunjid@gmail.com</p>
           </div>
           <a class="dropdown-item"><i class="dropdown-item-icon icon-user text-primary"></i> My Profile <span class="badge badge-pill badge-danger">1</span></a>
           <a class="dropdown-item"><i class="dropdown-item-icon icon-speech text-primary"></i> Messages</a>
